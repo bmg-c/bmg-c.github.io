245fa7ead6879eb9582da637e2d94fc19040125e
bmg-c <letalbark@gmail.com>
2022-01-15 22:14:47 +0600

    Changed the number

--- assets/js/copy.js
+++ assets/js/copy.js
@@ -7,8 +7,8 @@ function copyTextEmail() {
     });
 }
 function copyTextPhone() {
-    navigator.clipboard.writeText("87001385929").then(function() {
-        alert("Текст скопирован: 87001385929");
+    navigator.clipboard.writeText("87003385929").then(function() {
+        alert("Текст скопирован: 87003385929");
     }, function() {
         alert("Произошла ошибка, ваш браузер не поддерживает данную функцию. Вместо нажатия просто выделите(если на телефоне, то зажмите) и скопируйте.");
     });
