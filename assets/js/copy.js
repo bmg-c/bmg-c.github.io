c479eaa669eca902d80f4a5da17efcd788134207
bmg-c <letalbark@gmail.com>
2022-01-22 21:22:24 +0600

    Improvements to css and copy function
    - Stylized copy function
    - Added gradient colors
    - Added smooth scroll
    - Added normilize.css for compatibility

--- assets/js/copy.js
+++ assets/js/copy.js
@@ -1,15 +1,37 @@
 "use strict";
 function copyTextEmail() {
-    navigator.clipboard.writeText("avonavon_kz@mail.ru").then(function() {
-    alert("Текст скопирован: avonavon_kz@mail.ru");  
-    }, function() {
-        alert("Произошла ошибка, ваш браузер не поддерживает данную функцию. Вместо нажатия просто выделите(если на телефоне, то зажмите) и скопируйте.");
+    let aCont = document.querySelectorAll("#contact-cp-email");
+    aCont.forEach(a => {
+        a.addEventListener('click', event => {
+            let popup = event.target.firstElementChild;
+            navigator.clipboard.writeText("avonavon_kz@mail.ru").then(function() {
+                popup.textContent = "Скопированно!";
+            }, function() {
+                popup.textContent = "Произошла ошибка. Возможно ваш браузер не поддерживает данную функцию.";
+            });
+            popup.classList.toggle("show");
+            setTimeout(() => {
+                popup.classList.toggle("show");
+            }, 3500);
+        });
     });
 }
 function copyTextPhone() {
-    navigator.clipboard.writeText("87003385929").then(function() {
-        alert("Текст скопирован: 87003385929");
-    }, function() {
-        alert("Произошла ошибка, ваш браузер не поддерживает данную функцию. Вместо нажатия просто выделите(если на телефоне, то зажмите) и скопируйте.");
+    let aCont = document.querySelectorAll("#contact-cp-phone");
+    aCont.forEach(a => {
+        a.addEventListener('click', event => {
+            let popup = event.target.firstElementChild;
+            navigator.clipboard.writeText("+77003385929").then(function() {
+                popup.textContent = "Скопированно!"
+            }, function() {
+                popup.textContent = "Произошла ошибка. Возможно ваш браузер не поддерживает данную функцию."
+            });
+            popup.classList.toggle("show");
+            setTimeout(() => {
+                popup.classList.toggle("show");
+            }, 4000);
+        });
     });
 }
+copyTextEmail();
+copyTextPhone();
